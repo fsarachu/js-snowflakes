4dfc4ad27812f33f413a28354f279b0ccafcdc16
fsarachu <fsarachu@gmail.com>
2019-03-11 23:19:28 -0300

    Add snow!

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-import { greet } from "my-wasm-module";
+import { Snowflake } from "my-wasm-module";
 
-greet('Frankito');
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+document.body.appendChild(canvas);
+
+const flakesCount = 1000;
+const snowflakes = [];
+
+for(let s = 0; s < flakesCount; s++) {
+    snowflakes.push(Snowflake.new(window.innerWidth, window.innerHeight));
+}
+
+function update() {
+
+    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    for(const flake of snowflakes) {
+        flake.tick();
+        context.save();
+        context.beginPath();
+        context.arc(flake.x(), flake.y(), flake.radius(), 0, Math.PI * 2);
+        context.closePath();
+        context.fillStyle = '#FFF';
+        context.globalAlpha = flake.opacity();
+        context.fill();
+        context.restore();
+    }
+
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+
